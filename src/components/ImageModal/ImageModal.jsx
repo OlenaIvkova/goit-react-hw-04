e9fd6f5f2726555d7818d5210eaa38db03d99d42
Olena <alchimeraa@gmail.com>
2024-11-23 22:11:15 +0200

    components

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,29 +1,8 @@
-// import Modal from 'react-modal';
-// import React from 'react';
 import ReactModal from 'react-modal';
 import styles from './ImageModal.module.css';
 
 ReactModal.setAppElement('#root');
 
-// const ImageModal = ({ data, onClose }) => (
-//   <ReactModal
-//     isOpen={!!data} // Перевіряємо, чи передані дані для відображення
-//     onRequestClose={onClose}
-//     className={styles.modal}
-//     overlayClassName={styles.overlay}
-//     ariaHideApp={false} // Це потрібно тільки під час тестування, якщо setAppElement ще не налаштовано
-//   >
-//     <div className={styles.content}>
-//       <button className={styles.close} onClick={onClose}>
-//         ×
-//       </button>
-//       <img src={data.urls.regular} alt={data.alt_description || 'No description'} />
-//       <p>{data.alt_description || 'No description'}</p>
-//       <p>Author: {data.user.name}</p>
-//       <p>Likes: {data.likes}</p>
-//     </div>
-//   </ReactModal>
-// );
 
 const ImageModal = ({ data, onClose }) => (
   <ReactModal
